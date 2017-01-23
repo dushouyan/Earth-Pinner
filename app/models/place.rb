f8e9b928aee596bbe68d57719307f7c1464b2fda
Eric Falchier <ericfalchier@gmail.com>
2017-01-22 20:26:41 -0500

    added columns to places table, added geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,5 +7,12 @@ class Place < ApplicationRecord
 	def pretty_url
 		self.url = self.name.strip.downcase.gsub(' ','-')
 	end
-	
+
+	def full_street_address
+		[address, city, state, country].compact.join(', ')
+	end
+ 
+	geocoded_by :full_street_address
+	after_validation :geocode   
+
 end
--- db/migrate/20170122155509_add_column_to_places.rb
+++ db/migrate/20170122155509_add_column_to_places.rb
@@ -1,5 +1,8 @@
 class AddColumnToPlaces < ActiveRecord::Migration[5.0]
   def change
     add_column :places, :url, :string
+    add_column :places, :address, :string
+    add_column :places, :state, :string 
+    add_column :places, :created_by, :string 
   end
 end
