27263dce6c0c94d54826aaf6089849426629c8f1
Eric Falchier <ericfalchier@gmail.com>
2017-01-31 16:44:05 -0500

    user can delete account, session clears

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
+    session[:user_id] = nil
+    @user.delete
+    redirect_to '/'
   end
 
   def delete_dreams 
-      @user = User.find(params[:user_id])
-      @place = Place.find(params[:place_id])
-      @user.dream.places.delete(@place)
-      redirect_back(fallback_location: user_path(@user.id))
+    @user = User.find(params[:user_id])
+    @place = Place.find(params[:place_id])
+    @user.dream.places.delete(@place)
+    redirect_back(fallback_location: user_path(@user.id))
   end
 end
