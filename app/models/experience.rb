7c22c0899554d954569d17913f0ccc74143f319a
Eric Falchier <ericfalchier@gmail.com>
2017-02-01 11:36:48 -0500

    nil

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,6 +2,6 @@ class Experience < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
 
-	validates :review, presence: true, length: { minimum: 0, maximum: 1000 }
+	validates :review, presence: true
 	validates :rating, numericality: { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5 }
 end
