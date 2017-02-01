5579c5f76667fe75a425ff65c78f338e11674e2e
Eric Falchier <ericfalchier@gmail.com>
2017-02-01 12:10:30 -0500

    comments work again after commenting out numerical validations

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,6 +2,6 @@ class Experience < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
 
-	validates :review, presence: true
-	validates :rating, numericality: { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5 }
+	validates :review, presence: true #add lengths
+	#validates :rating, numericality: { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5 }
 end
