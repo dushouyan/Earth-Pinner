0ed90b1bb7b9a261e8c8f9b6280856a8ff75d89d
Eric Falchier <ericfalchier@gmail.com>
2017-03-28 10:38:53 +0300

    update omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.name = auth.info.name
-      user.admin = false 
+      user.name = auth.info.name 
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.admin = false
       user.save!
         if user.save
           Dream.create(user_id: user.id)
